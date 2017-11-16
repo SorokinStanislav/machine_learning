6cc5547ab7c7ffbbe5c63a30ad3474ce62617364
StanislavSorokin <ssss4-281094@yandex.ru>
2017-11-16 16:07:35 +0300

    task1 in lab2 done

---
+++ lab2/__init__.py
@@ -0,0 +1,33 @@
+import csv
+import matplotlib.pyplot as plt
+from matplotlib.ticker import AutoMinorLocator
+
+from lab2.task1 import task1
+
+
+def read_data():
+    with open('resources/data_v2-08.csv', newline='') as f:
+        reader = csv.reader(f)
+        next(reader, None)
+        x = []
+        for row in reader:
+            x.append(float(row[0]))
+        return x
+
+
+def visualize_input_data(input_data):
+    plt.figure(1)
+    ax = plt.axes()
+    ax.xaxis.set_minor_locator(AutoMinorLocator(2))
+    plt.legend()
+    plt.plot(input_data, len(input_data) * [1], "x", marker='x', color='red')
+    plt.show()
+
+
+data = read_data()
+visualize_input_data(data)
+task1(data)
+
+exit(0)
+
+
---
+++ lab2/task1.py
@@ -0,0 +1,114 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.ticker import AutoMinorLocator
+
+
+def box_kernel(u):
+    if 1 > u > -1:
+        return 0.5
+    else:
+        return 0
+
+
+def gaussian_kernel(u):
+    return 1 / np.math.sqrt(2*np.math.pi) * np.math.exp(-u ** 2 / 2)
+
+
+def epanechnikov_kernel(u):
+    if 1 > u > -1:
+        return 0.75 * (1 - u ** 2)
+    else:
+        return 0
+
+
+def triangular_kernel(u):
+    u_abs = np.math.fabs(u)
+    if u_abs < 1:
+        return 1 - u_abs
+    else:
+        return 0
+
+
+def interquartile_range(down_quartile_val, up_quartile_val):
+    return up_quartile_val - down_quartile_val
+
+
+def down_quartile(sorted_data):
+    l = len(sorted_data)
+    down = sorted_data[int((2 + l) / 4)]
+    up = sorted_data[np.math.ceil((2 + l) / 4)]
+    return (down + up) / 2
+
+
+def up_quartile(sorted_data):
+    l = len(sorted_data)
+    down = sorted_data[int((2 + 3 * l) / 4)]
+    up = sorted_data[np.math.ceil((2 + 3 * l) / 4)]
+    return (down + up) / 2
+
+
+def silverman_bandwidth(data):
+    sorted_data = sorted(data)
+    return 0.9 * min(np.std(data), interquartile_range(down_quartile(sorted_data), up_quartile(sorted_data)) / 1.34) * np.math.pow(len(data), -0.2)
+
+
+def general_naive_density_estimator(x, data, bandwidth, kernel_function):
+    n = len(data)
+    kernel_sum = 0
+    for i in range(n):
+        kernel_sum += kernel_function((x - data[i]) / bandwidth)
+    return 1 / (n * bandwidth) * kernel_sum
+
+
+def task1(data):
+    sorted_data = sorted(data)
+    silverman_band = silverman_bandwidth(data)
+    small_band = 0.1
+    large_band = 3
+    val = 0.
+
+    ins = []
+    silverman_outs = []
+    small_outs = []
+    large_outs = []
+    for x in sorted_data:
+        ins.append(x)
+        silverman_outs.append(general_naive_density_estimator(x, data, silverman_band, box_kernel))
+        small_outs.append(general_naive_density_estimator(x, data, small_band, box_kernel))
+        large_outs.append(general_naive_density_estimator(x, data, large_band, box_kernel))
+
+    plt.figure(2)
+    ax = plt.axes()
+    ax.yaxis.set_minor_locator(AutoMinorLocator(2))
+    ax.xaxis.set_minor_locator(AutoMinorLocator(2))
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    plt.plot(ins, small_outs)
+    plt.plot(sorted_data, np.zeros_like(sorted_data) + val, 'x', color='orange')
+    plt.xlabel('x')
+    plt.ylabel('f(x)')
+    plt.show()
+
+    plt.figure(3)
+    ax = plt.axes()
+    ax.yaxis.set_minor_locator(AutoMinorLocator(2))
+    ax.xaxis.set_minor_locator(AutoMinorLocator(2))
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    plt.plot(ins, silverman_outs)
+    plt.plot(sorted_data, np.zeros_like(sorted_data) + val, 'x', color='orange')
+    plt.xlabel('x')
+    plt.ylabel('f(x)')
+    plt.show()
+
+    plt.figure(4)
+    ax = plt.axes()
+    ax.yaxis.set_minor_locator(AutoMinorLocator(2))
+    ax.xaxis.set_minor_locator(AutoMinorLocator(2))
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    plt.plot(ins, large_outs)
+    plt.plot(sorted_data, np.zeros_like(sorted_data) + val, 'x', color='orange')
+    plt.xlabel('x')
+    plt.ylabel('f(x)')
+    plt.show()
